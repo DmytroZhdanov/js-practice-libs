427d67a73d46130e436828fb6128a536f6978fff
DmytroZhdanov <119117057+DmytroZhdanov@users.noreply.github.com>
2023-05-03 14:38:18 +0200

    Solves task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,15 @@
+import Player from '@vimeo/player';
+const throttle = require('lodash.throttle');
 
+const iframe = document.querySelector('iframe');
+const iframePlayer = new Player(iframe);
+const playerCurrentTime =
+  JSON.parse(localStorage.getItem('videoplayer-current-time')) || 0;
+
+iframePlayer.on('timeupdate', throttle(onTimeupdate, 1000));
+iframePlayer.setCurrentTime(playerCurrentTime);
+
+function onTimeupdate(data) {
+  const timeToStore = JSON.stringify(data.seconds);
+  localStorage.setItem('videoplayer-current-time', timeToStore);
+}
